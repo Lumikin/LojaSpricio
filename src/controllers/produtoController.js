e48d2488b5fd97d6f81a24a918dc048771487b9f
Lumikin <lumikomio04@gmail.com>
2025-10-20 14:52:59 -0300

    Adicionando uma nova função no produtoController

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -11,18 +11,33 @@ const produtoController = {
          * @param {*} res Objeto de resposta HTTP
          * @returns {Promise<Array<<object>>} Conteudo com os dados da requisição
          */
+
         try {
             const resultado = await produtoModel.selecionarTodos();
-            if(resultado.length === 0){
-                return res.status(200).json({ message: 'A tabela não contém dados'})
+            if (resultado.length === 0) {
+                return res.status(200).json({ message: 'A tabela não contém dados' })
 
             }
-            res.status(200).json({message: 'Resultado dos dados listados', data: resultado })
+            res.status(200).json({ message: 'Resultado dos dados listados', data: resultado })
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Ocorreu um erro no servidor', errorMessage: error.message })
         }
+    },
+    buscarProdutoId: async (req, res) => {
+        try {
+            const id = req.params.idProduto
+            if (!id || !Number.isInteger(id)) {
+                response.status(400).json({ Message: 'Forneça um indentificador (ID) valido' })
+            }
+            const resultadoId = await produtoModel.selecionarPorId(id);
+            res.status(200).json({ Message: 'Resultados dos dados listados', data: resultadoId })
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ message: 'Ocorreu um erro no servidor', errorMessage: error.message })
+        }
     }
 };
+
 
 export { produtoController }
