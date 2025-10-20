92e5d9e6fce3a28330c65fe6cd9409e8aa3c1fad
Lumikin <lumikomio04@gmail.com>
2025-10-20 15:07:29 -0300

    Adicionando o arquivo routes.js

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+//referencia do arquivo de rotas
+const { produtoRoutes } = require('./prdutoRoutes')
+
+
+router.use('/', produtoRoutes);
+
+module.exports = { router }
