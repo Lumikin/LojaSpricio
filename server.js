a4daefb75d3d08d4baed1137ef118d65c831580a
Lumikin <lumikomio04@gmail.com>
2025-10-20 15:09:25 -0300

    Atualizando o server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const { router } = require('./src/routes/routes')
 const app = express()
 const PORT = 8081
 
-
+app.use('/', router)
 
 app.use(express.json())
 app.listen(PORT, () => {
