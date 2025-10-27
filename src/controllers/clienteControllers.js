b6a995eab8860cf17319ae3a66ecbf34b788bd0d
Lumikin <lumikomio04@gmail.com>
2025-10-27 16:11:36 -0300

    Adicionado os codigos

--- src/controllers/clienteControllers.js
+++ src/controllers/clienteControllers.js
@@ -2,7 +2,7 @@
 
 const { clienteModel } = require('../models/clienteModel')
 const clienteController = {
-    SelecionarTodosClientes: async (req, res) => {
+    selecionarTodosClientes: async (req, res) => {
         try {
             const resultado = await clienteModel.selecionarTodos();
             if (resultado.length === 0) {
@@ -14,6 +14,32 @@ const clienteController = {
             res.status(500).json({ message: 'Ocorreu um erro no servidor', errorMessage: error.message })
         }
     },
+    adicionarCliente: async (req, res) => {
+        //TODO: Arrumar o Verificador de CPF
+        //TODO: Ver por que numeral esta sendo recusado!
+        try {
+            const { nome, cpf } = req.body;
+            if (!nome || nome.length < 3 || !String(nome) || !Number(cpf) ||cpf.length != 11) {
+                return res.status(400).json({ message: 'Dados invalidos' })
+            }
+            const consultarCPF = await clienteModel.selecionarTodos()
+            if (consultarCPF === cpf) {
+                return res.status(409).json({ message: "Cpf já cadastrado!" })
+
+            }
+            console.log(consultarCPF)
+            const resultado = await clienteModel.inserirCliente(nome, cpf)
+            if (resultado.affectedRows === 1 && resultado.insertId != 0) {
+                res.status(201).json({ message: 'Registro incluido com sucesso', result: resultado })
+            } else {
+                throw new Error('ocorreu um erro ao incluir o registro')
+            }
+            console.log(cpf)
+        } catch (error) {
+            console.error(error)
+            res.status(500).json({ Message: 'Ocorreu um erro no servidor.', errorMessage: error.message })
+        }
+    }
 
 }
 
--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const clienteRoutes = express.Router()
 const { clienteController } = require('../controllers/clienteControllers');
 
-clienteRoutes.get('/clientes/', clienteController.SelecionarTodosClientes)
-
+clienteRoutes.get('/clientes/', clienteController.selecionarTodosClientes)
+clienteRoutes.post('/clientes/', clienteController.adicionarCliente)
 module.exports = { clienteRoutes };
